870a0e159e8f16d216dba1a8ab860bdd8d01dfd5
Stephen Hilt <6362482+sjhilt@users.noreply.github.com>
2021-07-04 13:21:19 -0400

    Create graph_types.py

---
+++ graph_types.py
@@ -0,0 +1,42 @@
+import sqlite3
+import time
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+
+#connect to the hc911.db 
+conn = sqlite3.connect('hc911.db')
+c = conn.cursor()
+
+# Graph the Data from the Database
+def graph_data():
+    # SQLite Query to get the top 10 types for 2021 in this example 
+    c.execute('SELECT type,count(type) FROM events WHERE time LIKE \'%/2021%\' GROUP BY type ORDER BY count(type) DESC LIMIT 10 ')
+    # Read all results 
+    data = c.fetchall()
+    # setup lists for the data 
+    types = []
+    counts = []
+    
+    # Put the SQLite results into the two lists we will use for graphing 
+    for row in data:
+        types.append(row[0])
+        counts.append(row[1])
+        
+    # Create and build the Graph 
+    ax = plt.subplot(111)
+    # Set Y label and the Title 
+    ax.set(ylabel='Count by Types', title='Top 10 Counts by HC911 Type')
+    # set the x ticks to be numbers based on the types 
+    ax.set_xticks(np.arange(len(types)))
+    # set the x axis lables to be the types 
+    ax.set_xticklabels(types)
+    # create the graph based off the x and y values from the database
+    ax.bar(np.arange(len(types)), counts)
+    # show the graph
+    plt.show()
+
+# Call the graph function and close the connections to the database. 
+graph_data()
+c.close
+conn.close()
