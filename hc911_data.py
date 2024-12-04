e920dd0c79720694b1e082f9c98c196068898c3a
Stephen Hilt <6362482+sjhilt@users.noreply.github.com>
2024-12-03 21:19:20 -0500

    Update hc911_data.py
    added more fields to the database

--- hc911_data.py
+++ hc911_data.py
@@ -3,9 +3,10 @@
 #  Author: Ben Stoker
 #  Original Author: Stephen Hilt
 #  Purpose: To convert hc911.org alerts to SQLite database entries
-#  Version: 0.1
+#  Version: 0.2
 #  Notes: (4/19/2018) Initial Build
 #         (11/15/2024) merged with the slack tools one that has the new URL
+#         (12/03/2024) Updated to include additional fields from the API response
 #
 ########################################
 from pprint import pprint
@@ -18,65 +19,62 @@ import os
 import re
 import sqlite3
 
-# also post to sqllite.db 
-def post_sqlite(time, type, event, responder, area, address):
+# Function to insert data into the SQLite database
+def post_sqlite(time, type, event, responder, area, address, latitude, longitude, city, state, priority, agency_type, battalion):
     conn = sqlite3.connect('hc911.db')
     c = conn.cursor()
-    c.execute('INSERT INTO events (time, type, event, responder, area, address) VALUES (?,?,?,?,?,?)', (time, type, event, responder, area, address))
+    # Insert event data into the database
+    c.execute('''
+        INSERT INTO events (
+            time, type, event, responder, area, address, 
+            latitude, longitude, city, state, priority, agency_type, battalion
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (time, type, event, responder, area, address, latitude, longitude, city, state, priority, agency_type, battalion))
     conn.commit()
     conn.close()
 
-  
-#Change a list to a string, might need to do this a few times 
+# Change a list to a string
 def listToString(s):
-    # initialize an empty string
     str1 = " "
-    # return string 
-    return (str1.join(s))
+    return str1.join(s)
 
-#URL AND API SETUP
-#New Website as of 2022-09-06
+# URL and API Setup
 url = "https://hc911server.com/api/calls"
-#GET the website from requests
 response = requests.get(url)
 
-#Get 24 hour time for Hour of the current time
+# Get the current time in 24-hour format
 hour = strftime("%H")
-# minute minus 5, the time between 'created_str' and current time is around a 5min delta
-min = int(strftime("%M")) -5
-# if after the -5 we have a negative number then we need to make this the previous hour
+min = int(strftime("%M")) - 5
 if min < 0:
-        min = 60 - abs(min)
-        hour = str(int(hour) - 1)
+    min = 60 - abs(min)
+    hour = str(int(hour) - 1)
 min = "{0:0=2d}".format(min)
-# Look at the response from the GET requests
+
+# Process each event in the API response
 for dic in response.json():
-        # pull the creation time and save it as time 
-        time = dic['creation']
-        # new time is the changing the date, and time (T) is the seperator 
-        newtime = time.split("T")[1]
-        # year will be saved as n_year 
-        n_year = time.split("T")[0]
-        # hour will be saved as n_hour 
-        n_hour = newtime.split(":")[0]
-        # minutes will be saved as n_min
-        n_min = newtime.split(":")[1]
-        # seconds will be saved as n_seconds 
-        n_seconds = newtime.split(":")[2].split(".")[0]
-        # create a time variable to use for the messages 
-        time = n_year + " " + n_hour + ":" + n_min + ":" + n_seconds
-        # save type_description as type 
-        type = dic['type_description']
-        # save the status as event
-        event = dic['status']
-        # save the jurisdiction as responder 
-        responder = dic['jurisdiction']
-        # save the cross streets as area 
-        area = dic['crossstreets']
-        # save location as address 
-        address = dic['location'] 
- 
-        # IF the time in hours and minuts match the 5 mins ago hour and min then
-        if(n_hour == str(hour)and n_min == str(min)): 
-            # insert the event into the SQLite3 database 
-            post_sqlite(time, type, event, responder, area, address)
+    # Extract and format the creation time
+    time = dic['creation']
+    newtime = time.split("T")[1]
+    n_year = time.split("T")[0]
+    n_hour = newtime.split(":")[0]
+    n_min = newtime.split(":")[1]
+    n_seconds = newtime.split(":")[2].split(".")[0]
+    time = f"{n_year} {n_hour}:{n_min}:{n_seconds}"
+
+    # Extract additional fields from the API response
+    type = dic['type_description']
+    event = dic['status']
+    responder = dic['jurisdiction']
+    area = dic['crossstreets']
+    address = dic['location']
+    latitude = dic.get('latitude', None)
+    longitude = dic.get('longitude', None)
+    city = dic.get('city', None)
+    state = dic.get('state', None)
+    priority = dic.get('priority', None)
+    agency_type = dic.get('agency_type', None)
+    battalion = dic.get('battalion', None)
+
+    # Check if the event matches the time criteria
+    if n_hour == str(hour) and n_min == str(min):
+        post_sqlite(time, type, event, responder, area, address, latitude, longitude, city, state, priority, agency_type, battalion)
